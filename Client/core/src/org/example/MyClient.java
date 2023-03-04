65c53d9cfe00ddf1b41fe76758b9c7d037eb62d0
tatall <tatall@ttu.ee>
2023-03-04 15:37:03 +0200

    added 2d thing,not work, trying on pc

--- Client/core/src/org/example/MyClient.java
+++ Client/core/src/org/example/MyClient.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -8,7 +10,27 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MyClient {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import java.util.Arrays;
+
+import static com.badlogic.gdx.Gdx.input;
+
+
+public class MyClient extends ApplicationAdapter {
 
     private final Client client;
 
@@ -30,7 +52,9 @@ public class MyClient {
                     Player[] playersList = (Player[]) object;
 
                     // print the game state into the console
-                    printGame(playersList);
+                    // printGame(playersList);
+                    // new Basic3DTest().create();
+                    create();
                 }
             }
         });
@@ -51,7 +75,7 @@ public class MyClient {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.next();
             Character direction = input.charAt(0);  // We only get the first character.
-
+            //render();
             /**
              * Send this movement to the server.
              * The server should move "my player" and then send the updated board to all players.
@@ -81,4 +105,97 @@ public class MyClient {
         }
         System.out.println("Enter direction to move in (NESW):");
     }
+    private Player[] players;
+    private TextField inputField;
+    private Label[][] boardLabels;
+    private Stage stage;
+
+    public void create() {
+        // Set up the stage
+        stage = new Stage(new ScreenViewport());
+        input.setInputProcessor(stage);
+
+        // Create the game board
+        String[][] board = new String[10][10];
+        Arrays.stream(board).forEach(a -> Arrays.fill(a, "."));
+
+        for (Player player : players) {
+            board[player.y][player.x] = "O";
+        }
+
+        // Create the skin
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        // Create the UI components
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        Label boardLabel = new Label("Current board:", skin);
+        boardLabel.setFontScale(2f);
+        boardLabel.setAlignment(Align.center);
+        table.add(boardLabel).colspan(10).padBottom(10);
+        table.row();
+
+        boardLabels = new Label[10][10];
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                Label label = new Label(board[row][col], skin);
+                label.setFontScale(2f);
+                label.setAlignment(Align.center);
+                label.setColor(Color.WHITE);
+                table.add(label).width(50).height(50);
+                boardLabels[row][col] = label;
+            }
+            table.row();
+        }
+
+        Label promptLabel = new Label("Enter direction to move in (NESW):", skin);
+        promptLabel.setFontScale(2f);
+        table.add(promptLabel).colspan(10).padTop(10);
+        table.row();
+
+        inputField = new TextField("", skin);
+        table.add(inputField).colspan(10);
+    }
+
+    public void render() {
+        // Update the game board
+        String[][] board = new String[10][10];
+        Arrays.stream(board).forEach(a -> Arrays.fill(a, "."));
+
+        for (Player player : players) {
+            board[player.y][player.x] = "O";
+        }
+
+        // Update the board labels
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                boardLabels[row][col].setText(board[row][col]);
+            }
+        }
+
+        // Handle input
+        if (input.isKeyPressed(Input.Keys.ENTER)) {
+            String input = inputField.getText();
+        }
+        if (input.toString().length() > 0) {
+            // Process the input
+            // ...
+
+            // Clear the input field
+            inputField.setText("");
+        }
+
+        // Clear the screen and draw the UI
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+    }
+
+    public void dispose() {
+        stage.dispose();
+    }
+
+
 }
